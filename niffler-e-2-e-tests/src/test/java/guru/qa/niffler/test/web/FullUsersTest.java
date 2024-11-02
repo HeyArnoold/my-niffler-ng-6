45cc3da63f85028d45c0171d8c8c9fb4f275735e
Antonov Maksim <maks.antonov.2012@gmail.com>
2024-11-02 22:10:10 +0300

    ДЗ 13

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FullUsersTest.java
@@ -0,0 +1,23 @@
+package guru.qa.niffler.test.web;
+
+import guru.qa.niffler.jupiter.annotation.User;
+import guru.qa.niffler.model.UserJson;
+import guru.qa.niffler.service.impl.api.UserApiClient;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@Order(Integer.MAX_VALUE)
+public class FullUsersTest {
+
+    @User
+    @Test
+    void shouldReturnNotEmptyUsersListTest(UserJson user) {
+        UserApiClient usersApiClient = new UserApiClient();
+        List<UserJson> response = usersApiClient.allUsers(user.username(), null);
+        assertFalse(response.isEmpty(), "Список пользователей должен быть не пустым");
+    }
+}
