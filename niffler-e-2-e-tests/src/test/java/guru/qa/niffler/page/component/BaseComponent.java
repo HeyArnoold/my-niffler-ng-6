c3360afb0bb1d68688f1258513b8bfbac537ce8c
Antonov Maksim <maks.antonov.2012@gmail.com>
2024-11-02 21:56:26 +0300

    рефакторинг по замечаниям

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/component/BaseComponent.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/component/BaseComponent.java
@@ -2,17 +2,11 @@ package guru.qa.niffler.page.component;
 
 import com.codeborne.selenide.SelenideElement;
 
-import javax.annotation.Nonnull;
-
 public abstract class BaseComponent<T extends BaseComponent<?>> {
 
     protected final SelenideElement self;
 
     public BaseComponent(SelenideElement self) {
         this.self = self;
-    }
-
-    public @Nonnull SelenideElement getSelf() {
-        return self;
     }
 }
