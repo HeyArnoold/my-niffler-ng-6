9aff4a1d15bfb6a12a4a6df112ec20762230bc49
Antonov Maksim <maks.antonov.2012@gmail.com>
2024-11-15 19:51:29 +0300

    Рефакторинг + влил изменения из урока 10.1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -7,61 +7,68 @@ import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
+import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.model.UserJson;
+import guru.qa.niffler.page.EditSpendingPage;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
+import org.junit.jupiter.api.Test;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+
+import static guru.qa.niffler.utils.RandomDataUtils.genRandomCategory;
+import static guru.qa.niffler.utils.RandomDataUtils.genRandomSentence;
 
 @WebTest
 class SpendingWebTest {
 
     private static final Config CFG = Config.getInstance();
 
-//    @User(
-//            username = "duck",
-//            spendings = @Spending(
-//                    category = "Обучение",
-//                    description = "Обучение Advanced 2.0",
-//                    amount = 79990
-//            )
-//    )
-//    @Test
-//    void categoryDescriptionShouldBeChangedFromTable(SpendJson spend) {
-//        final String newDescription = "Обучение Niffler Next Generation";
-//
-//        Selenide.open(CFG.frontUrl(), LoginPage.class)
-//                .login("duck", "12345")
-//                .editSpending(spend.description())
-//                .setNewSpendingDescription(newDescription)
-//                .save()
-//                .search(newDescription)
-//                .checkTableContainsSpendingByDescription(newDescription);
-//    }
-//
-//    @User
-//    @Test
-//    void addSpendTest(UserJson user) {
-//        String category = genRandomCategory();
-//        String description = genRandomSentence(2);
-//
-//        EditSpendingPage spendingPage = Selenide.open(CFG.frontUrl(), LoginPage.class)
-//                .login(user.username(), user.testData().password())
-//                .addSpending();
-//
-//        MainPage mainPage = spendingPage
-//                .setSpendingCategory(category)
-//                .setNewSpendingDescription(description)
-//                .setSpendingAmount("10")
-//                .setDate(new Date())
-//                .save()
-//                .checkAlertMessage("New spending is successfully created");
-//
-//        mainPage
-//                .checkTableContainsSpendingByDescription(description);
-//    }
+    @User(
+            username = "duck",
+            spendings = @Spending(
+                    category = "Обучение",
+                    description = "Обучение Advanced 2.0",
+                    amount = 79990
+            )
+    )
+    @Test
+    void categoryDescriptionShouldBeChangedFromTable(SpendJson spend) {
+        final String newDescription = "Обучение Niffler Next Generation";
+
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login("duck", "12345")
+                .editSpending(spend.description())
+                .setNewSpendingDescription(newDescription)
+                .save()
+                .search(newDescription)
+                .checkTableContainsSpendingByDescription(newDescription);
+    }
+
+    @User
+    @Test
+    void addSpendTest(UserJson user) {
+        String category = genRandomCategory();
+        String description = genRandomSentence(2);
+
+        EditSpendingPage spendingPage = Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.testData().password())
+                .addSpending();
+
+        MainPage mainPage = spendingPage
+                .setSpendingCategory(category)
+                .setNewSpendingDescription(description)
+                .setSpendingAmount("10")
+                .setDate(new Date())
+                .save()
+                .checkAlertMessage("New spending is successfully created");
+
+        mainPage
+                .checkTableContainsSpendingByDescription(description);
+    }
 
     @User(
             spendings = @Spending(
